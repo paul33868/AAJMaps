ec91df16371836ba5805f86819a245b077db48e4
paul33868 <gil.a.pablo@gmail.com>
2015-06-23 12:44:13 -0300

    Initial working maps app
    We get it done!

---
+++ AAJMaps/AAJMaps/MapPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace AAJMaps
+{
+    public class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            var map = new Map(
+                MapSpan.FromCenterAndRadius(
+                        new Position(-32.9595, -60.661541), Distance.FromMiles(0.3)))
+                        {
+                            IsShowingUser = true,
+                            HeightRequest = 100,
+                            WidthRequest = 960,
+                            VerticalOptions = LayoutOptions.FillAndExpand,
+                            MapType = MapType.Street
+                        };
+
+            var stack = new StackLayout { Spacing = 0 };
+            stack.Children.Add(map);
+            Content = stack;
+        }
+    }
+}
